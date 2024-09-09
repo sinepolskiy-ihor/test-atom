76c8b3d6afc9cff73b3b4ca2099496a3d4150f6f
Ігор Синєпольський <sinepolskiyihor@gmail.com>
2024-09-09 11:07:11 +0300

    fix submenu found

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ menuSearchInput.addEventListener(`input`, () =>
     menuElems.forEach(elem =>
     {
         elem.parentNode.classList.remove(`d-none`)
+        elem.parentNode.classList.remove(`d-dropdown-found`)
         if(elem.innerText)
         {
             const found = elem.innerText.toLowerCase().includes(menuSearchInput.value.toLowerCase())
             const closestDropDown = elem.closest(`.nav-item.dropdown`)
-            if(menuSearchInput.value && !found)
+            if(menuSearchInput.value && !found && !elem.parentNode.classList.contains(`d-dropdown-found`))
             {
                 elem.parentNode.classList.add(`d-none`)
             }
             if(menuSearchInput.value && found && elem.parentNode.classList.contains(`nav-item`) && elem.parentNode.classList.contains(`dropdown`))
             {
-                elem.parentNode.querySelectorAll(`ul > li`).forEach(elemChild => elemChild.classList.remove(`d-none`))   
+                elem.parentNode.querySelectorAll(`ul > li`).forEach(elemChild =>
+                {
+                    elemChild.classList.remove(`d-none`)
+                    elemChild.classList.add(`d-dropdown-found`)
+                })   
             }
             if(menuSearchInput.value && found && closestDropDown)
             {
